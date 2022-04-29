68940418391876564319b8d2d40537f877c979b4
Miao Zhao <miao_zhao@yahoo.com>
2022-04-29 20:54:26 +1000

    created file for Exercises: Variables and User Input

---
+++ variables_and_user_input/exercise_variables_and_input.py
@@ -0,0 +1,25 @@
+# Q1
+number1 = input('Enter a number: ')
+number2 = input('Enter another number: ')
+print(float(number1) + float(number2))
+
+# Q2
+number1_q2 = input('Enter a number: ')
+number2_q2 = input('Enter another number: ')
+print(float(number1_q2) * float(number2_q2))
+
+# Q3
+distance_km_input = input('How many kilometres? ')
+distance_km = float(distance_km_input)
+distance_m = int(distance_km * 1000)
+distance_cm = int(distance_km * 100000)
+print_m = f"{distance_km}km = {distance_m}m"
+print_cm = f"{distance_km}km = {distance_cm}cm"
+print(print_m)
+print(print_cm)
+
+# Q3
+name = input('What is your name? ')
+height = input('How tall are you (cms)? ')
+output = f"{name} is {height}cms tall."
+print(output)
