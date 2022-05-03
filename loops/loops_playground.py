35cba7ca607e83e30fb4102d1a48f44e4e4ac0dc
Miao Zhao <miao_zhao@yahoo.com>
2022-05-04 08:17:03 +1000

    loops class for and while loop

---
+++ loops/loops_playground.py
@@ -0,0 +1,60 @@
+# 1111
+# for i in range(1, 6): # default staring int is 0 (if we don't put 1 at the staring, it will start from 0)
+#     print(i)
+
+# 2222
+# numbers = [10, 20, 30, 40]
+# for number in numbers:
+#     print(number)
+
+# 3333
+# for num in range(10):
+#     print(num)
+
+# 4444
+# for num in range(1,11):
+#     print(num)
+
+# 5555
+# for num in range(0, 11, 2): # the last two is by how many to skip one in between
+#     print(num)
+
+# 6666
+# chilli_wishlist = ['igloo', 'chicken', 'donut toy', 'cardboard']
+# for item in range(len(chilli_wishlist)):
+#     print(item)
+
+# 7777
+# chilli_wishlist = ['igloo', 'chicken', 'donut toy', 'cardboard']
+# for item in range(len(chilli_wishlist)):
+#     print(chilli_wishlist[item])
+
+# 8888
+# chilli_wishlist = [
+#     ["igloo"],
+#     ["donut toy", "tennis ball", "croc toy"],
+#     ["chicken", "peanut butter"],
+#     ["cardboard box", "kong", "dig mat"],
+# ]
+# for category in chilli_wishlist:
+#     for item in (range(len(category))):
+#         print(category[item])
+
+# 9999 while loop
+# while 5 > 3:
+#     print("hi")
+# num = 1
+# while num > 0:
+#     print('Hi')
+#     num = 0
+
+# 10
+# guess =''
+# while guess != "a":
+#     guess = input('Guess a letter: ')
+
+# 11
+counter = 10
+while counter <= 15:
+    print(counter)
+    counter = counter +1
