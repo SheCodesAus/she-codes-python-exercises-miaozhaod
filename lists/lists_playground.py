e6cb28990963a0d74e0f4647fa551bbfc80eebda
Miao Zhao <miao_zhao@yahoo.com>
2022-05-03 18:26:08 +1000

    the list class

---
+++ lists/lists_playground.py
@@ -0,0 +1,18 @@
+# Lists
+characters = ["a", "b", "c"]
+# print(characters[0])
+
+characters.append('d')
+characters.extend(['e', 'f'])
+# characters.insert(1, 'z')
+# print(characters[-1])
+# print(characters[0:2])
+# print(characters.pop(2))
+# print(characters.remove('a'))
+# print(characters)
+
+chilli_wishlist = ['igloo', 'chicken', 'donut toy', 'cardboard box']
+# chilli_wishlist[1] = 'carrot'
+# print(chilli_wishlist)
+print(chilli_wishlist[2:4])
+print(chilli_wishlist[-3])
