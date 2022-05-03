d69071921b044fec4a3803d65e30c7849d40fe4a
Miao Zhao <miao_zhao@yahoo.com>
2022-05-04 08:17:41 +1000

    Create excercise_loop_extensions.py

---
+++ loops/excercise_loop_extensions.py
@@ -0,0 +1,23 @@
+# groceries = [
+#     ["Baby Spinach", 2.78],
+#     ["Hot Chocolate", 3.70],
+#     ["Crackers", 2.10],
+#     ["Bacon", 9.00],
+#     ["Carrots", 0.56],
+#     ["Oranges", 3.08],
+# ]
+
+# baby_spinach = input("Number of Baby Spinach: ")
+# hot_chocolate = input("Number of Hot Chocolate: ")
+# crackers = input("Number of Crackers: ")
+# bacon = input("Number of Bacon: ")
+# carrots = input("Number of carrots: ")
+# ornages = input("Number of oranges: ")
+# all_ammounts = [int(baby_spinach), int(hot_chocolate), int(crackers), int(bacon), int(carrots), int(ornages)]
+
+# for grocery in groceries:
+#     print(grocery.append(2))
+
+list = [1, 2, 3]
+list.append(4)
+print(list)
