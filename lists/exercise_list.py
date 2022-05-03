a0846f338f2b364bd807fbc9898a8d3d2fe7b7f8
Miao Zhao <miao_zhao@yahoo.com>
2022-05-03 18:26:20 +1000

    the list exercise

---
+++ lists/exercise_list.py
@@ -0,0 +1,48 @@
+# Q1
+foods = [
+    "orange",
+    "apple",
+    "banana",
+    "strawberry",
+    "grape",
+    "blueberry",
+    ["carrot", "cauliflower", "pumpkin"],
+    "passionfruit",
+    "mango",
+    "kiwifruit",
+]
+# print(foods[0])
+# print(foods[2])
+# print(foods[-1])
+# print(foods[0:3])
+# print(foods[-3:])
+# print(foods[-4][-1])
+
+# Q2
+mailing_list = [
+    ["Chilli", "chilli@thechihuahua.com"],
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+    ["Ivy", "noreply@goldendreamers.xyz"],
+]
+# for item in mailing_list:
+#     print(f"{item[0]}: {item[1]}")
+
+# Q3
+# name1 = input("Enter a name: ")
+# name2 = input("Enter a name: ")
+# name3 = input("Enter a name: ")
+# output = [name1, name2, name3]
+# print(output)
+
+# Q4
+# google searched append multiple lists python to get the list resolution
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 8, 9]
+d = [a, b, c]
+e = a + b + c   
+print(d)
+print(e)
+
