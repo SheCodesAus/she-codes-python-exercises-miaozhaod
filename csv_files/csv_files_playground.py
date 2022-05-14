fd3466899e9b843e4bdbb10ebff32a9e0c9f4abd
Miao Zhao <miao_zhao@yahoo.com>
2022-05-14 10:57:16 +1000

    Update csv_files_playground.py

--- csv_files/csv_files_playground.py
+++ csv_files/csv_files_playground.py
@@ -14,10 +14,10 @@ with open("2016_pilbara.csv") as csv_file:
     for line in reader:
         print(line)
 
-population = []
+# population = []
 
-with open("2016_pilbara.csv", encoding="utf-8") as csv_file:
-    reader = csv.reader(csv_file)
-    for line in reader:
-        population.append(line)
-print(population)
+# with open("2016_pilbara.csv", encoding="utf-8") as csv_file:
+#     reader = csv.reader(csv_file)
+#     for line in reader:
+#         population.append(line)
+# print(population)
